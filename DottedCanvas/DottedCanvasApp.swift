681a35c2226153ad75fd1a940181c9d70d9a7a9f
EisukeKusachi <eisukekusachi@gmail.com>
2023-08-25 00:20:04 +0900

    Initial Commit

---
+++ DottedCanvas/DottedCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  DottedCanvasApp.swift
+//  DottedCanvas
+//
+//  Created by Eisuke Kusachi on 2023/08/25.
+//
+
+import SwiftUI
+
+@main
+struct DottedCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
