01a32606c29da1052032bf0a7a09c8331f1904db
EisukeKusachi <eisukekusachi@gmail.com>
2023-12-01 21:12:18 +0900

    Moved save, load, and new creation functionalities to MainView
    Moved saveProject() to MainView
    Moved opening loadingView to MainView
    Moved opening snackBar to MainView
    Moved creating a new canvas to MainView

--- DottedCanvas/LayerView/LayerView.swift
+++ DottedCanvas/LayerView/LayerView.swift
@@ -9,108 +9,79 @@ import SwiftUI
 
 struct LayerView: View {
     @ObservedObject var mainViewModel: MainViewModel
-    @Binding var isProjectListViewPresented: Bool
+
     @Binding var isProjectsEmpty: Bool
 
     @State var isSubImageViewPresented: Bool = false
-    @State var isVisibleLoadingView: Bool = false
-    @State var isVisibleSnackbar: Bool = false
-    @State var isZippingCompleted: Bool = false
-    @State var isNewImageAlertPresented: Bool = false
+
+    var saveProject: () -> Void
+    var loadProject: () -> Void
+    var newProject: () -> Void
 
     @State var selectedSubImage = SubImageModel()
 
     @State var message: String = ""
 
     var body: some View {
-        ZStack {
-            VStack {
-                ToolbarView(
-                    mainViewModel: mainViewModel,
-                    isProjectsEmpty: $isProjectsEmpty,
-                    addSubLayer: {
-                        isSubImageViewPresented = true
-                        updateSubLayer()
-                    },
-                    removeSubLayer: {
-                        mainViewModel.removeSelectedSubLayer()
-                        updateSubLayer()
-                    },
-                    saveProject: {
-                        let zipFileName = mainViewModel.projectName + "." + "\(Output.zipSuffix)"
-                        saveProject(zipFileURL: URL.documents.appendingPathComponent(zipFileName))
-                    },
-                    loadProject: {
-                        isProjectListViewPresented = true
-                    },
-                    newProject: {
-                        isNewImageAlertPresented = true
-                    }
-                )
-
-                Spacer()
-                    .frame(height: 12)
-
-                PreviewView(image: $mainViewModel.mergedSubLayerImage,
-                            diameter: min(UIScreen.main.bounds.size.width * 0.8, 500))
-
-                if mainViewModel.subLayers.isEmpty {
-                    Spacer()
-                    Text("Tap the + button to create a new image.")
-                    Spacer()
-
-                } else {
-                    SubLayerList(
-                        viewModel: mainViewModel,
-                        selectedSubImageAlpha: $mainViewModel.selectedSubImageAlpha,
-                        didSelectLayer: { _ in
-                            updateSubLayer()
-                    })
+        VStack {
+            ToolbarView(
+                mainViewModel: mainViewModel,
+                isProjectsEmpty: $isProjectsEmpty,
+                addSubLayer: {
+                    isSubImageViewPresented = true
+                    updateSubLayer()
+                },
+                removeSubLayer: {
+                    mainViewModel.removeSelectedSubLayer()
+                    updateSubLayer()
+                },
+                saveProject: {
+                    saveProject()
+                },
+                loadProject: {
+                    loadProject()
+                },
+                newProject: {
+                    newProject()
                 }
-            }
-            .padding()
+            )
 
-            if isVisibleLoadingView {
-                LoadingDialog(isVisibleLoadingView: $isVisibleLoadingView,
-                              message: $message)
-                    .onDisappear {
-                        isVisibleSnackbar = true
-                    }
-            }
+            Spacer()
+                .frame(height: 12)
 
-            if isVisibleSnackbar {
-                Snackbar(isDisplayed: $isVisibleSnackbar,
-                         imageSystemName: "hand.thumbsup.fill",
-                         comment: isZippingCompleted ? "Success" : "Error")
+            PreviewView(image: $mainViewModel.mergedSubLayerImage,
+                        diameter: min(UIScreen.main.bounds.size.width * 0.8, 500))
+
+            if mainViewModel.subLayers.isEmpty {
+                Spacer()
+                Text("Tap the + button to create a new image.")
+                Spacer()
+
+            } else {
+                SubLayerList(
+                    viewModel: mainViewModel,
+                    selectedSubImageAlpha: $mainViewModel.selectedSubImageAlpha,
+                    didSelectLayer: { _ in
+                        updateSubLayer()
+                })
             }
         }
+        .padding()
         .sheet(isPresented: $isSubImageViewPresented) {
             SubImageView(isViewPresented: $isSubImageViewPresented,
                          data: selectedSubImage) { data, image in
 
                 updateMainImage(newSubImageData: data,
                                 dotImage: image)
+
                 mainViewModel.selectedSubImageAlpha = data.alpha
             }
         }
-        .alert(isPresented: $isNewImageAlertPresented) {
-            let title = "Confirm"
-            let message = ["If you create a new project, ",
-                           "any existing work will be deleted.",
-                           "\n",
-                           "Are you sure you want to continue?"
-                            ].joined()
-            let action = {
-                mainViewModel.reset()
+        .onReceive(mainViewModel.$selectedSubLayer, perform: { subLayer in
+            if subLayer == nil {
                 selectedSubImage = SubImageModel()
             }
-
-            return Alert(title: Text(title),
-                         message: Text(message),
-                         primaryButton: .default(Text("OK"),
-                                                 action: action),
-                         secondaryButton: .destructive(Text("Cancel")))
-        }
+        })
     }
 
     private func updateMainImage(newSubImageData: SubImageModel,
@@ -127,43 +98,6 @@ struct LayerView: View {
             selectedSubImage = SubImageModel(layerData: subLayerData)
         }
     }
-
-    private func saveProject(zipFileURL: URL) {
-        /*
-        guard let data = mainViewModel.dottedCanvasData else { return }
-        Task {
-            do {
-                let startDate = Date()
-                let projectName = zipFileURL.fileName!
-
-                message = "Saving..."
-                isVisibleLoadingView = true
-                isZippingCompleted = false
-                try await Task.sleep(nanoseconds: UInt64(1 * 1000))
-
-                try projectListViewModel.saveData(mainModel: data,
-                                                  zipFileURL: zipFileURL)
-
-                projectListViewModel.upsertData(projectName: projectName,
-                                                newThumbnail: data.mainImageThumbnail)
-
-                let sleep: CGFloat = 1.0 - Date().timeIntervalSince(startDate)
-                if sleep > 0.0 {
-                    try await Task.sleep(nanoseconds: UInt64(sleep * 1000 * 1000 * 1000))
-                }
-
-                isVisibleLoadingView = false
-                isZippingCompleted = true
-
-            } catch {
-                showAlert(error)
-            }
-        }
-        */
-    }
-    private func showAlert(_ error: Error) {
-        print(error)
-    }
 }
 
 struct LayerView_Previews: PreviewProvider {
@@ -173,11 +107,18 @@ struct LayerView_Previews: PreviewProvider {
                                .init(title: "Title 1", alpha: 225, isVisible: false),
                                .init(title: "Title 2", alpha: 25)
             ])
-        @State var isProjectListViewPresented: Bool = false
         @State var isProjectsEmpty: Bool = false
 
         LayerView(mainViewModel: mainViewModel,
-                  isProjectListViewPresented: $isProjectListViewPresented,
-                  isProjectsEmpty: $isProjectsEmpty)
+                  isProjectsEmpty: $isProjectsEmpty,
+                  saveProject: {
+            print("saveProject")
+        },
+                  loadProject: {
+            print("loadProject")
+        },
+                  newProject: {
+            print("newProject")
+        })
     }
 }
--- DottedCanvas/MainView.swift
+++ DottedCanvas/MainView.swift
@@ -12,37 +12,117 @@ struct MainView: View {
     @StateObject var projectListViewModel = ProjectListViewModel()
     @State var isProjectListViewPresented: Bool = false
 
+    @State var message: String = ""
+    @State var isVisibleLoadingView: Bool = false
+    @State var isVisibleSnackbar: Bool = false
+    @State var isZippingCompleted: Bool = false
+
+    @State var isNewImageAlertPresented: Bool = false
+
     var body: some View {
-        LayerView(mainViewModel: mainViewModel,
-                  isProjectListViewPresented: $isProjectListViewPresented,
-                  isProjectsEmpty: projectListViewModel.isProjectsEmptyBinding)
-        .sheet(isPresented: $isProjectListViewPresented) {
-            ProjectListView(
-                isViewPresented: $isProjectListViewPresented,
-                viewModel: projectListViewModel,
-                didSelectItem: { index in
-                    let projectName = projectListViewModel.projects[index].projectName
-                    let zipFileURL = URL.documents.appendingPathComponent(projectName + ".zip")
-                    loadProject(zipFileURL: zipFileURL)
-                })
-        }
-        .onAppear {
-            Task {
-                do {
-                    let urls = URL.documents.allURLs
-                    let projects = try await loadListProjectDataArray(from: urls)
-
-                    DispatchQueue.main.async {
-                        self.projectListViewModel.projects = projects
-                    }
-
-                } catch {
-                    throw error
+        ZStack {
+            LayerView(mainViewModel: mainViewModel,
+                      isProjectsEmpty: projectListViewModel.isProjectsEmptyBinding,
+                      saveProject: {
+                let zipFileName = mainViewModel.projectName + "." + "\(Output.zipSuffix)"
+                saveProject(zipFileURL: URL.documents.appendingPathComponent(zipFileName))
+            },
+                      loadProject: {
+                isProjectListViewPresented = true
+            },
+                      newProject: {
+                isNewImageAlertPresented = true
+            })
+            .sheet(isPresented: $isProjectListViewPresented) {
+                ProjectListView(
+                    isViewPresented: $isProjectListViewPresented,
+                    viewModel: projectListViewModel,
+                    didSelectItem: { index in
+                        let projectName = projectListViewModel.projects[index].projectName
+                        let zipFileURL = URL.documents.appendingPathComponent(projectName + ".zip")
+                        loadProject(zipFileURL: zipFileURL)
+                    })
+            }
+            .alert(isPresented: $isNewImageAlertPresented) {
+                let title = "Confirm"
+                let message = ["If you create a new project, ",
+                               "any existing work will be deleted.",
+                               "\n",
+                               "Are you sure you want to continue?"
+                ].joined()
+                let action = {
+                    mainViewModel.reset()
                 }
+                
+                return Alert(title: Text(title),
+                             message: Text(message),
+                             primaryButton: .default(Text("OK"),
+                                                     action: action),
+                             secondaryButton: .destructive(Text("Cancel")))
+            }
+            .onAppear {
+                Task {
+                    do {
+                        let urls = URL.documents.allURLs
+                        let projects = try await loadListProjectDataArray(from: urls)
+                        
+                        DispatchQueue.main.async {
+                            self.projectListViewModel.projects = projects
+                        }
+                        
+                    } catch {
+                        throw error
+                    }
+                }
+            }
+            
+            if isVisibleLoadingView {
+                LoadingDialog(isVisibleLoadingView: $isVisibleLoadingView,
+                              message: $message)
+                .onDisappear {
+                    isVisibleSnackbar = true
+                }
+            }
+            
+            if isVisibleSnackbar {
+                Snackbar(isDisplayed: $isVisibleSnackbar,
+                         imageSystemName: "hand.thumbsup.fill",
+                         comment: isZippingCompleted ? "Success" : "Error")
             }
         }
     }
 
+    private func saveProject(zipFileURL: URL) {
+        guard let data = mainViewModel.dottedCanvasData else { return }
+        Task {
+            do {
+                let startDate = Date()
+                let projectName = zipFileURL.fileName!
+
+                message = "Saving..."
+                isVisibleLoadingView = true
+                isZippingCompleted = false
+                try await Task.sleep(nanoseconds: UInt64(1 * 1000))
+
+                try projectListViewModel.saveData(mainModel: data,
+                                                  zipFileURL: zipFileURL)
+
+                projectListViewModel.upsertData(projectName: projectName,
+                                                newThumbnail: data.mainImageThumbnail)
+
+                let sleep: CGFloat = 1.0 - Date().timeIntervalSince(startDate)
+                if sleep > 0.0 {
+                    try await Task.sleep(nanoseconds: UInt64(sleep * 1000 * 1000 * 1000))
+                }
+
+                isVisibleLoadingView = false
+                isZippingCompleted = true
+
+            } catch {
+                showAlert(error)
+            }
+        }
+    }
     private func loadProject(zipFileURL: URL) {
         do {
             let projectData = try mainViewModel.loadData(fromZipFileURL: zipFileURL)
